bb6286d5585226b192c25ac09bee3c5d7f7a9fd8
nwj002 <nwj.shrestha@gmail.com>
2022-11-25 11:38:54 +0545

    push

--- database/DbConnection.java
+++ database/DbConnection.java
@@ -1,99 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package database;
-
-import javax.swing.*;
-
 import java.sql.*;
 
+
 public class DbConnection {
-
-    public Connection connection;
-
-    Statement statement;
-
-    ResultSet resultSet;
-
-    int value;
-
-    public DbConnection() {
-
-        try {
-
-            String username = "root";
-
-            String password = "2002";
-
+    public Connection con;
+            
+    public static void main(String[] args){
+        try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-
-            connection = DriverManager.getConnection(
-
-                    "jdbc:mysql://localhost:3306/SoftwaricaDB", username, password);
-
-            if (connection != null) {
-
-                System.out.println("Connected to database --> SoftwaricaDB");
-
-            } else {
-
-                System.out.println("Error connecting to database");
-
+            Connection con=DriverManager.getConnection("jdbc:mysql://sql6.freesqldatabase.com:3306/sql6580070","sql6580070","6VVrAkhI5w");
+            
+            if(con!=null){
+                System.out.println("Successfully connected");
             }
-
-            statement = connection.createStatement();
-
-        } catch (Exception e) {
-
-            e.printStackTrace();
-
+            
+        }catch(Exception e){
+            System.out.println(e);
+            System.out.println("Not connected");
+            
         }
-
     }
-
-    // Via the use of sql query
-
-    // insert, update and delete
-
-    public int manipulate(String query) {
-
-        try {
-
-            value = statement.executeUpdate(query);
-
-            connection.close();
-
-        } catch (SQLIntegrityConstraintViolationException ex) {
-
-            JOptionPane.showMessageDialog(null, "These details already exist!");
-
-        } catch (SQLException e) {
-
-            e.printStackTrace();
-
-        }
-
-        return value;
-
-    }
-
-    public ResultSet retrieve(String query) {
-
-        try {
-
-            resultSet = statement.executeQuery(query);
-
-        } catch (SQLException e) {
-
-            e.printStackTrace();
-
-        }
-
-        return resultSet;
-
-    }
-
-    public static void main(String[] args) {
-
-        new DbConnection();
-
-    }
-
+    
+    
+    
 }
