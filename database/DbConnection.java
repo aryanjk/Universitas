e9a759bf502a192210caa40e211ef74f55a84033
krishalsatta <97888056+krishalsatta@users.noreply.github.com>
2022-11-26 16:27:28 +0545

    Add files via upload

--- database/DbConnection.java
+++ database/DbConnection.java
@@ -1,37 +1,99 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package database;
+
+import javax.swing.*;
+
 import java.sql.*;
 
-/**
- *
- * @author ujjwalpariyar
- */
-public class database {
-    public Connection con;
-            
-    public static Connection connectdb(){
-        try{
+public class DbConnection {
+
+    public Connection connection;
+
+    Statement statement;
+
+    ResultSet resultSet;
+
+    int value;
+
+    public DbConnection() {
+
+        try {
+
+            String username = "sql6580070";
+
+            String password = "6VVrAkhI5w";
+
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con=DriverManager.getConnection("jdbc:mysql://sql6.freesqldatabase.com:3306/sql6580070","sql6580070","6VVrAkhI5w");
-            
-            if(con!=null){
-                System.out.println("Successfully connected");
+
+            connection = DriverManager.getConnection(
+
+                    "jdbc:mysql://sql6.freesqldatabase.com:3306/sql6580070", username, password);
+
+            if (connection != null) {
+
+                System.out.println("Connected to database --> universitas");
+
+            } else {
+
+                System.out.println("Error connecting to database");
+
             }
-            
-        }catch(Exception e){
-            System.out.println(e);
-            System.out.println("Not connected");
-            
+
+            statement = connection.createStatement();
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
         }
-        return null;
+
     }
+
+    // Via the use of sql query
+
+    // insert, update and delete
+
+    public int manipulate(String query) {
+
+        try {
+
+            value = statement.executeUpdate(query);
+
+            connection.close();
+
+        } catch (SQLIntegrityConstraintViolationException ex) {
+
+            JOptionPane.showMessageDialog(null, "These details already exist!");
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+
+        }
+
+        return value;
+
+    }
+
+    public ResultSet retrieve(String query) {
+
+        try {
+
+            resultSet = statement.executeQuery(query);
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+
+        }
+
+        return resultSet;
+
+    }
+
     public static void main(String[] args) {
-        database.connectdb();
+
+        new DbConnection();
+
     }
-    
-    
-    
+
 }
