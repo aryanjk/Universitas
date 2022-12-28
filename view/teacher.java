ad8c85b7da66e28a8fadde4ae75b7d50dff7faa6
seetall <105225591+seetall@users.noreply.github.com>
2022-12-28 21:10:08 +0545

    view: add teacher

--- view/teacher.java
+++ view/teacher.java
@@ -190,7 +190,6 @@ public class teacher extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(teacher.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
