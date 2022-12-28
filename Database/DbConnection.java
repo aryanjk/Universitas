e794958aa14ece51d83c178bbc95cea7ee870bb8
Ujjse <104347000+Ujjse@users.noreply.github.com>
2022-12-28 14:06:03 +0545

    Database for student viewing

--- Database/DbConnection.java
+++ Database/DbConnection.java
@@ -14,37 +14,47 @@ public class DbConnection {
 
     int value;
 
-    public DbConnection() {
+    public static Connection getconnection() {
 
         try {
 
-            String username = "universitas";
 
-            String password = "Universitas121";
+            final String username = "universitas";
+
+            final String password = "30dc48eb";
+
 
             Class.forName("com.mysql.cj.jdbc.Driver");
 
-            connection = DriverManager.getConnection(
+           Connection connection = DriverManager.getConnection(
 
-                    "jdbc:mysql://db4free.net:3306/universitas", "universitas", "Universitas121");
+
+                    "jdbc:mysql://db4free.net :3306/universitas", username, password);
+
+           
 
             if (connection != null) {
+                
 
-                System.out.println("Connected to database --> universitas");
-
-            } else {
+                System.out.println("Connected to database --> Universitas Database");
+                return connection;
+                
+              } else {
 
                 System.out.println("Error connecting to database");
 
             }
+            
 
-            statement = connection.createStatement();
+            
 
         } catch (Exception e) {
 
             e.printStackTrace();
 
         }
+        return null;
+        
 
     }
 
@@ -55,6 +65,7 @@ public class DbConnection {
     public int manipulate(String query) {
 
         try {
+            statement = connection.createStatement();
 
             value = statement.executeUpdate(query);
 
@@ -74,26 +85,20 @@ public class DbConnection {
 
     }
 
-    public ResultSet retrieve(String query) {
-
+    public ResultSet retrieve(PreparedStatement st){
         try {
-
-            resultSet = statement.executeQuery(query);
-
-        } catch (SQLException e) {
-
+            resultSet=st.executeQuery();
+        }catch (SQLException e){
             e.printStackTrace();
-
         }
-
         return resultSet;
-
     }
 
     public static void main(String[] args) {
 
-        new DbConnection();
+         DbConnection.getconnection();
 
     }
 
-}
+   }
+
